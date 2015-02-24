cc9631e0c439d5047c28e3031868e3c95a7d2f96
LimeStreem <LimeStreem@gmail.com>
2015-02-24 16:04:25 +0900

    commit to version1

---
+++ FeatureValueExperiment/WrappedImage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlimDX;
+using SlimDX.Direct3D11;
+
+namespace FeatureValueExperiment
+{
+    public class WrappedImage:IDisposable
+    {
+        private readonly Texture2D _texture;
+        private FeatureValueContext context;
+
+        private DataBox dataBox;
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public WrappedImage(Texture2D texture, FeatureValueContext context)
+        {
+            _texture = texture;
+            this.context = context;
+            dataBox = context.device.ImmediateContext.MapSubresource(texture, 0, 0, MapMode.Read, MapFlags.None);
+            this.Height = texture.Description.Height;
+            this.Width = texture.Description.Width;
+        }
+
+        public unsafe Vector4 this[int x, int y]
+        {
+            get
+            {
+                byte* fPtr=(byte*)(dataBox.Data.DataPointer+(4*y*Width + x*4));
+                return new Vector4(*fPtr,*(fPtr+1),*(fPtr+2),*(fPtr+3));
+            }
+        }
+
+
+
+        public void Dispose()
+        {
+            context.device.ImmediateContext.UnmapSubresource(_texture,0);
+        }
+
+
+    }
+}
---
+++ FeatureValueExperimentExecutable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FeatureValueExperiment;
+
+namespace FeatureValueExperimentExecutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FeatureValueContext context=new FeatureValueContext();
+            var data = context.GetWrappedImage(@"C:\Users\LimeStreem\Pictures\ee9f30179b25b4a265df6536f05d38ca.png");
+            var pix = data[100, 100];
+        }
+    }
+}
