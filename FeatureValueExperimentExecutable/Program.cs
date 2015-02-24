2114a2ac1845c96325275893d1c8d969bdcaf9b1
LimeStreem <LimeStreem@gmail.com>
2015-02-24 16:10:13 +0900

    add usage

--- FeatureValueExperimentExecutable/Program.cs
+++ FeatureValueExperimentExecutable/Program.cs
@@ -12,8 +12,18 @@ namespace FeatureValueExperimentExecutable
         static void Main(string[] args)
         {
             FeatureValueContext context=new FeatureValueContext();
-            var data = context.GetWrappedImage(@"C:\Users\LimeStreem\Pictures\ee9f30179b25b4a265df6536f05d38ca.png");
-            var pix = data[100, 100];
+            using (var data = context.GetWrappedImage(@"C:\Users\LimeStreem\Pictures\ee9f30179b25b4a265df6536f05d38ca.png"))
+            {
+                for (int x = 0; x < data.Width; x++)
+                {
+                    for (int y = 0; y < data.Height; y++)
+                    {
+                        var d = data[x, y];
+                        Console.WriteLine("(X,Y)=({0},{1})=RGB({2},{3},{4})",x,y,d.X,d.Y,d.Z);
+                    }
+                }
+            }
+            
         }
     }
 }
